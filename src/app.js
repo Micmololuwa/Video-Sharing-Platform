b4fc3bf9287139616db3ad549b1abe86685608e5
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-11 23:47:06 +0100

    continuos update

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react"
+import navBar from "./components/navbar"
+
+export default function App(){
+    return (
+        <div>
+            <navBar />
+        </div>
+        
+    )
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function navBar(){
+    return (
+        <div className="navbar">
+            <img src="./src/images/menu"/>
+        </div>
+    )
+}
