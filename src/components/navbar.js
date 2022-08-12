851b72d09b7b6c3c61947b0b8c49e85dd439224e
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-12 22:31:26 +0100

    changed images folder location to public folder

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,36 @@
 import React from "react"
+import "../css/style.css"
 
-export default function navBar(){
+export default function NavBar(){
     return (
         <div className="navbar">
-            <img src="./src/images/menu"/>
+            <p>I love myself</p>
+
+            <div className="left-side">
+                
+                <img src="./images/menu.png"/>
+                <div className="logo">
+                    <img src="./images/youtube.png" />
+                    <h1 className="youtube-text"> YouTube</h1>
+                </div>
+            </div>
+            
+
+            <div className="middle">
+
+                <form className="search">
+                    <input placeholder="search" />
+                    <img src="././images/search.png" />
+                </form>
+                <img src="././images/mic.png" />
+
+            </div>
+            
+            <div className="right-side">
+                <img src="././images/video.png" />
+                <img src="././images/bell.png" />
+                <img src="././images/profile.png" />
+            </div>
         </div>
     )
 }
