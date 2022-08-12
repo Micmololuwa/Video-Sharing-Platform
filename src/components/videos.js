209b36496d13fa98fc0e3aee9a846366fc7dce7a
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-13 00:31:41 +0100

    added videos component and video image thumbnail.

---
+++ src/components/videos.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Videos() {
+  return (
+    <div className='videos'>
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+            <div className='poster-img'>
+                <img src='./images/profile.png' />
+            </div>
+            <div className='info'>
+                <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+                <p>Echo Room</p>
+                <p>14k views . 1 day ago</p>
+            </div>
+        </div>
+    </div>
+  )
+}
