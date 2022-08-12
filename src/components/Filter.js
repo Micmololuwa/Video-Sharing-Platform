25aa48d29cb055eead385c5a38152f0ba211d91e
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-13 00:11:05 +0100

    Added the filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../css/style.css"
+
+export default function Filter() {
+  return (
+    <div className='filter'>
+        <button>All</button>
+        <button>Live</button>
+        <button>Music</button>
+        <button>Mixes</button>
+        <button>JavaScript</button>
+        <button>User interface design</button>
+        <button>UX</button>
+        <button>Python</button>
+        <button>Graphic design</button>
+        <button>Jenifa</button>
+        <button>Sports leagues</button>
+        <button>History</button>
+    </div>
+  )
+}
