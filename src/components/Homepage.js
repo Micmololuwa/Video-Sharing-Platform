4fa950349b7f62046a9c18ba27b0346773fb59a0
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-13 10:32:33 +0100

    Added Homepage which include the filter and video

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Filter from './Filter'
+import Videos from './videos'
+
+export default function HomePage() {
+  return (
+    <div className= "HomePage" >
+        <Filter />
+        <Videos />
+    </div>
+  )
+}
