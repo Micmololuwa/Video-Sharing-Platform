7955d6ca19ae0d9bd30fba8ebce82bf4a11f9f1c
Michael Mololuwa Stephen <molux825@gmail.com>
2022-08-17 21:25:53 +0100

    final changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,23 +7,23 @@ export default function SideBar (){
         <div className="sidebar">
 
             <a href="#" className="home">
-                <img src="./images/home.png" />
+                <img src="./images/home.png" alt = ""/>
                 <p>Home</p>
             </a>
             <a href="#" className="explore">
-                <img src="./images/explore.png" />
+                <img src="./images/explore.png" alt = "" />
                 <p>Explore</p>
             </a>
             <a href="#" className="short">
-                <img src="./images/short.png" />
+                <img src="./images/short.png" alt = "" />
                 <p>Short</p>
             </a>
             <a href="#" className="subscription">
-                <img src="./images/subscription.png" />
+                <img src="./images/subscription.png" alt = "" />
                 <p>Subscription</p>
             </a>
             <a href="#" className="library">
-                <img src="./images/library.png" />
+                <img src="./images/library.png" alt = "" />
                 <p>Library</p>
             </a>
             
--- src/components/videos.js
+++ src/components/videos.js
@@ -4,18 +4,141 @@ import "./css/style.css"
 export default function Videos() {
   return (
     <div className='videos'>
+      <div className = "video">
         <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
 
         <div className='video-info'>
-            <div className='poster-img'>
-                <img src='./images/profile.png' />
-            </div>
-            <div className='info'>
-                <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
-                <p>Echo Room</p>
-                <p>14k views . 1 day ago</p>
-            </div>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
         </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "video">
+        <div className='thumbnail'> <img alt='thumnail' src='./images/youtubethumbnail.png' /> </div> 
+
+        <div className='video-info'>
+          <div className='poster-img'>
+            <img src='./images/profile.png' />
+          </div>
+
+          <div className='info'>
+            <h3>Bella Shmurda - New Born Fela | ECHOOROOM LIVE...</h3>
+            <p>Echo Room</p>
+            <p>14k views . 1 day ago</p>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
